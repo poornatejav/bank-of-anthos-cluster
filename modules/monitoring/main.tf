d32ef85d8113b32d6f0614c958c572315006df3c
poornatejav <poornateja340@gmail.com>
2025-05-21 15:12:03 -0500

    edit

--- modules/monitoring/main.tf
+++ modules/monitoring/main.tf
@@ -1,3 +1,26 @@
+provider "kubernetes" {
+  host                   = var.cluster_endpoint
+  cluster_ca_certificate = base64decode(var.cluster_ca_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = var.cluster_endpoint
+    cluster_ca_certificate = base64decode(var.cluster_ca_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    }
+  }
+}
+
 resource "helm_release" "kube_prometheus_stack" {
   name             = "monitoring"
   repository       = "https://prometheus-community.github.io/helm-charts"
