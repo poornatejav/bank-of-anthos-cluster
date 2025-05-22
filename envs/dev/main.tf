c76d40717692ff6357cf06e5328ea7b8dbf4dba2
poornatejav <poornateja340@gmail.com>
2025-05-21 20:22:14 -0500

    update path

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -45,9 +45,9 @@ module "gitops" {
 
 
 module "ingress_nginx" {
-  source = "./helm_ingress_nginx"
+  source = "../../helm_ingress_nginx"
 }
 
 module "ingress_rules" {
-  source = "./ingress_rules"
+  source = "../../ingress_rules"
 }
